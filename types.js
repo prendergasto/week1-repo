a1584b7aa6e5fdc5a474a702aade84f311d6aef9
Owen Prendergast <prendergasto@gmail.com>
2020-03-09 15:37:55 -0700

    some stuff working

---
+++ types.js
@@ -0,0 +1,43 @@
+const isNumber = val => typeof val === 'number';
+const isString = str => typeof str === 'string';
+const isBoolean = bool => typeof bool === 'boolean';
+const isArray = arr = Array.isArray() === true;
+const isObject = obj = Object.prototype.toString.call() === '[object Object]';
+
+
+const castToNumber = val => {
+if(isNumber(val)) return val;
+const number = Number(val);
+if(isNaN(number)) throw new CastError(Number, val);
+return number;
+};
+
+console.log(castToNumber);
+
+class CastError extends Error {
+constructor(Type, value) {
+    const type = Type.name;
+    super(`Cannot cast >>${value}<< to ${type}`);
+    this.type = type;
+    this.value = value;
+}
+}
+
+const casters = {
+Number: castToNumber,
+};
+
+const getCaster = Type => {  
+return casters[Type.name] || null;
+};
+
+module.exports = {
+isNumber,
+isString,
+isBoolean,
+isArray,
+isObject,
+CastError,
+getCaster,
+castToNumber,
+};
